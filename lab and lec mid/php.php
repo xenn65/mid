129d98570064fc5a1184583e6212dec949c932a7
xenn65 <114554488+xenn65@users.noreply.github.com>
2022-12-13 22:18:50 +0800

    Add files via upload

---
+++ lab and lec mid/php.php
@@ -0,0 +1,96 @@
+<?php
+
+// Global Variables $firstname = "John Paul";
+
+$grade = 11;
+
+$price = 11.1;
+
+$question = true;
+
+// Rendered Variables
+
+echo $firstname . "<br>" . $grade. "<br>" . $price;
+
+// arithmetic
+
+echo "<br>"S
+
+echo 4 + 4;
+
+echo "<br>";
+
+echo 4 - 4;
+
+echo "<br>";
+
+echo 4 * 4;
+
+echo "<br>";
+
+echo 4 / 4;
+
+echo "<br>";
+
+echo 4 % 4;
+
+echo "<br>";
+
+// for loops arithmetic
+
+for ($i=1; $i <= 10; $i++) { # code...
+
+echo $i;
+
+}
+
+echo "<br>";
+
+// if else
+
+I used for trappings
+
+$username = "jl";
+
+$password = "password";
+
+if ($username == "john paul" && $password "password") {
+
+echo "successful login";
+
+} else {
+
+echo "Wrong username/password";
+
+}
+
+echo "<br>";
+// for loops arithmetic
+
+for ($i=1; $i <= 10; $i++) { # code...
+
+echo $i;
+
+}
+
+echo "<br>";
+
+// if else
+
+I used for trappings
+
+$username = "jl";
+
+$password = "password";
+
+if ($username == "azurin026" && $password "password") {
+
+echo "successful login";
+
+} else {
+
+echo "Wrong username/password";
+
+}
+
+echo "<br>";
